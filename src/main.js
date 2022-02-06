ab96f406512bd1cd24c15e1111aebbe3ce0b78f3
Desaser <rihane.samy@gmail.com>
2022-02-06 18:08:16 +0100

    First

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
+import "bootstrap/dist/css/bootstrap.css"
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store';
+import router from './router';
 
-createApp(App).mount('#app')
+
+//importing AOS
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+createApp(App)
+.use(router)
+.use(store)
+.use(AOS.init())
+.mount('#app')
